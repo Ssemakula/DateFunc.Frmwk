f7fab32f25c58e6dd6be9dd918eb2dc05d15d7f9
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2024-11-03 09:14:30 +0300

    Copyright Ingormation. No functional change here

--- DateFunc.Frmwk/Properties/AssemblyInfo.cs
+++ DateFunc.Frmwk/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DateFunc.Frmwk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Date Methods")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Czemi")]
 [assembly: AssemblyProduct("DateFunc.Frmwk")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Czemi")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
